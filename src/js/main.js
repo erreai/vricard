f42aa4c7afb957405b12bd6fc07a115ab1df291a
Raimundo Concha <raimundoconcha@MacBook-Pro-de-Raimundo.local>
2025-11-03 13:47:09 -0300

    new slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+// Default theme
+import '@splidejs/splide/css';
 
 // Import all of Bootstrap’s JS
-import * as bs from 'bootstrap'
+import * as bootstrap from 'bootstrap'
+import Splide from '@splidejs/splide';
+new Splide( '.splide', {direction: 'ttb', height   : '580px'} ).mount();
