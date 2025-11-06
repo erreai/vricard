a087329f9ad33e425bbaec0d04eb260ca7bacb8b
Rai <rai.concha@gmail.com>
2025-11-05 21:07:13 -0300

    jumbotron home

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,17 @@ import '@splidejs/splide/css';
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
 import Splide from '@splidejs/splide';
-new Splide( '.splide', {direction: 'ttb', height   : '580px'} ).mount();
+new Splide( '.splide', {direction: 'ttb', height   : '710px', arrows: false, autoplay: 'pause'} ).mount();
+
+// Liquify jumbotron home
+if (document.getElementById('jumbotron')) {
+    let $jumbotrolSlides, $jumboImg, $jumboContent;
+    $jumbotrolSlides = document.querySelectorAll('li.jumbotron');
+    $jumbotrolSlides.forEach(element => {
+        $jumboImg = element.querySelector('img');
+        $jumboContent = element.querySelector('.splide_container');
+        const { attributes } = $jumboImg;
+        element.style.backgroundImage = 'url(' + attributes.src.nodeValue + ')';
+        $jumboContent.style.height = $jumboContent.parentElement.clientHeight + 'px';
+    });
+}
